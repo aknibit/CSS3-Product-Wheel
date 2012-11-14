a18efa2d9c682143cc7fbdb264eb81659026c18d
Jonathan <jonathan@aknibit.com>
2012-11-14 23:44:55 +0100

    upload

---
+++ index.php
@@ -0,0 +1,144 @@
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width">
+<link rel="stylesheet" href="style.css">
+
+<script>
+
+$(function(){
+   $(".r33").hover(
+        function(){
+        alert('hola');
+            $("#innerContainer").css('border-color','#FFF');
+            $("#outerContainer").css('border-color','#FFF');
+        },
+        function(){
+            $("#innerContainer").css('border-color','#000');
+            $("#outerContainer").css('border-color','#000');
+        }
+    );
+});
+
+
+</script>
+
+</head>
+<body> 
+
+<div class="pieContainer">
+
+<div class="pieBackground"></div>
+<div id="pieSliceGreen" class="hold"><div class="pie"></div></div> 
+<div id="pieSliceYellow" class="hold"><div class="pie"></div></div>
+<div id="pieSliceBlue" class="hold"><div class="pie"></div></div>
+<div id="pieSliceOrange" class="hold"><div class="pie"></div></div>
+
+<a href="" title="">
+<div class="tit_pie r360">
+<h3>REPORTS360º</h3>
+<p>Herramienta de<br />reports personalizados</p>
+</div>
+</a>
+<a href="" title="">
+<div class="tit_pie r340">
+<h3>CONTENT340º</h3>
+<p>Generación de<br />contenido social</p>
+</div>
+</a>
+<a href="" title="">
+<div class="tit_pie r320">
+<h3>METRICS320º</h3>
+<p>Analítica web</p>
+</div>
+</a>
+<a href="" title="">
+<div class="tit_pie r300">
+<h3>SOCIAL300º</h3>
+<p>Optimización de<br />redes sociales</p>
+</div>
+</a>
+<a href="" title="">
+<div class="tit_pie r280">
+<h3>MAIL280º</h3>
+<p>E-mail<br />marketing</p>
+</div>
+</a>
+<a href="" title="">
+<div class="tit_pie r260">
+<h3>FB260º</h3>
+<p>Desarrollo de páginas<br />y aplicaciones<br />en Facebook</p>
+</div>
+</a>
+<a href="" title="">
+<div class="tit_pie r240">
+<h3>SEM240º</h3>
+<p>Estrategias de pago<br />por click</p>
+</div>
+</a>
+<a href="" title="">
+<div class="tit_pie r220">
+<h3>SEO220º</h3>
+<p>Posicionamiento<br />web</p>
+</div>
+</a>
+<a href="" title="">
+<div class="tit_pie r200">
+<h3>START200º</h3>
+<p>Start-up's SEO</p>
+</div>
+</a>
+
+<a href="" title="">
+<div class="tit_pie r33">
+<h3>TYPO3º</h3>
+<p>Gestor de<br />contenidos</p>
+</div>
+</a>
+<a href="" title="">
+<div class="tit_pie r20">
+<h3>ANGULAR20º</h3>
+<p>Aplicaciones web<br />a medida</p>
+</div>
+</a>
+<a href="" title="">
+<div class="tit_pie r40">
+<h3>SHAPE40º</h3>
+<p>Mantenimiento integral<br />de su web</p>
+</div>
+</a>
+<a href="" title="">
+<div class="tit_pie r60">
+<h3>SHOP60º</h3>
+<p>Desarrollo de<br />tiendas online</p>
+</div>
+</a>
+<a href="" title="">
+<div class="tit_pie r80">
+<h3>INTRA80º</h3>
+<p>Programación de<br />intranets</p>
+</div>
+</a>
+<a href="" title="">
+<div class="tit_pie r120">
+<h3>DESIGN120º</h3>
+<p>Diseño web y gráfico</p>
+</div>
+</a>
+<a href="" title="">
+<div class="tit_pie r150">
+<h3>NATIVE150º</h3>
+<p>Aplicaciones nativas para smartphones</p>
+</div>
+</a>
+<a href="" title="">
+<div class="tit_pie r170">
+<h3>JMOBILE170º</h3>
+<p>Desarrollo de web móvil</p>
+</div>
+</a>
+
+</div>
+
+</body> 
+</html>
